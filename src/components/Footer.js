85a056edd1bf6ab5ccb6a3d39a71bc06ff7b043f
suryansh <suryanshpatwal@gmail.com>
2024-08-31 10:39:19 +0530

    updated projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Suryansh Patwal</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} SP</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
@@ -25,7 +25,7 @@ function Footer() {
               <a
                 href="https://github.com/StrugglerSuryansh"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
@@ -35,7 +35,7 @@ function Footer() {
               <a
                 href="https://twitter.com/CSuryansh"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiOutlineTwitter />
@@ -45,7 +45,7 @@ function Footer() {
               <a
                 href="https://www.linkedin.com/in/suryansh-patwal/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
@@ -55,7 +55,7 @@ function Footer() {
               <a
                 href="https://www.instagram.com/shhhhhsuryansh"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillInstagram />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+import neuro from "../../Assets/Projects/neuro.png";
 import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import watcher from "../../Assets/Projects/watcher.png";
+import dice from "../../Assets/Projects/Dice.png"
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import Design from "../../Assets/Projects/Design.png";
+
 
 function Projects() {
   return (
@@ -23,12 +24,12 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={neuro}
               isBlog={false}
-              title="loda lassan"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title=" Neurodiverse Education System"
+              description="An AI-driven platform designed to enhance learning for neurodiverse students using interactive games like Whack-a-Mole and Hangman, providing a personalized and engaging educational experience."
+              ghLink="https://github.com/StrugglerSuryansh/static-site"
+              demoLink="https://static-site-tan.vercel.app/Main/index.html"
             />
           </Col>
 
@@ -36,43 +37,44 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Queue Reduction System"
+              description="A system that uses facial recognition and blockchain technology to optimize queue management, reducing wait times and enhancing data security in public and private environments."
+              ghLink="https://github.com/StrugglerSuryansh/DiceGame"
+              demoLink="https://project-3-mu-brown.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={watcher}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Watch Party Web App"
+              description="A web application built with React.js, Vite, and ZegoCloud API that allows users to watch videos together in real time. It features synchronized viewing experiences through WebSockets and offers a smooth streaming experience."
+              ghLink="https://github.com/StrugglerSuryansh/watcher"
+              demoLink="https://watcher-01.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={Design}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Web designing"
+              description="Developed a visually stunning website using GSAP for smooth animations and mastered hosting to ensure global accessibility. The project aimed for Awwwards recognition by showcasing advanced front-end skills and design excellence."
+              ghLink="https://github.com/StrugglerSuryansh/project-2"
+              demoLink="https://strugglersuryansh.github.io/project-2/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={dice}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="The Dice Game"
+              description="Created a fun dice game project in React to master core concepts like state management and conditional rendering. Roll a dice, select a number, and score points based on matches. Designed and styled in Figma. 🚀"
+              ghLink="https://github.com/StrugglerSuryansh/DiceGame"
+              demoLink="https://project-3-mu-brown.vercel.app/"
+            // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
@@ -84,7 +86,7 @@ function Projects() {
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
         </Row>
