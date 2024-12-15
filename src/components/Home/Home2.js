43f03226040d94a27fdeff93062e8db8fc9092e5
suryansh <suryanshpatwal@gmail.com>
2024-12-15 12:38:05 +0530

    fixed ig

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/shhhhhsuryansh"
+                  href="https://www.instagram.com/suryaaansh_patwal/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
